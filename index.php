33d2e5dad369764f3b79872d9a857fd13e0b3ba0
BuildTools <unconfigured@null.spigotmc.org>
2020-04-16 20:35:27 +0200

    ...

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ update_user($update);
 
 if(isset($update['message']['new_chat_member']) AND $update['message']['chat']['id'] == WELCOME_CHAT)
 {
-  sendMessage(WELCOME_CHAT, WELCOME_MSG);
+  sendMessage(WELCOME_CHAT,"Willkommen <b>".$update['message']['new_chat_member']['first_name']."</b>".CR.WELCOME_MSG);
 
 }
 
