a480671764b50fee1f3c367dd7c001ec0989069d
BuildTools <unconfigured@null.spigotmc.org>
2020-04-16 14:51:52 +0200

    ...

--- mods/bossinfo.php
+++ mods/bossinfo.php
@@ -12,13 +12,13 @@ $user_id = $update['callback_query']['from']['id'];
 $file= IMAGE_PATH.$pokemon.'.png';
 error_log("DATEI: ".$file);
 $p = send_photo($user_id, $file, '...', array(), ['disable_web_page_preview' => 'true']);
-error_log($p)
+error_log($p);
 
 
 $file= "https://www.novitas-bkk.de/_daten/fotos/startseitenbilder/sl_leistungen2020.jpg";
 error_log("DATEI: ".$file);
 $p = send_photo($user_id, $file, '...', array(), ['disable_web_page_preview' => 'true']);
-error_log($p)
+error_log($p);
 
 
  ?>
