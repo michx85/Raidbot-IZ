460975d5dd3156edf1a20df1c5730bcfd89a6bc1
BuildTools <unconfigured@null.spigotmc.org>
2020-04-17 11:02:47 +0200

    ...

--- mods/bossinfo.php
+++ mods/bossinfo.php
@@ -9,7 +9,7 @@ $pokemon = $data['arg'];
 $user_id = $update['callback_query']['from']['id'];
 
 
-$file= "https://raidbot-test.piraidgruppe.de/index.php?bossinfo=true&pic=".$pokemon;
+$file= "https://raidbot-test.piraidgruppe.de/index.php?bossinfo=true&pic=".$pokemon.'&nocache='.time();
 $p = send_photo($user_id, $file, '...', array(), ['disable_web_page_preview' => 'true']);
 
 
