c7e7e1207a430176e73f0f19371ba96b8506a5e4
BuildTools <unconfigured@null.spigotmc.org>
2020-04-20 00:00:34 +0200

    ...

--- core/tools/iz_import.php
+++ core/tools/iz_import.php
@@ -3,6 +3,18 @@
 
 include_once(dirname(__DIR__). '/bot/requirements.php');
 
+// Datenbankverbindung
+if($db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
+{
+
+	$db->set_charset('utf8mb4');
+
+}
+else
+{
+	logging("Datenbank-Verbindung gescheitert.","mysql_connect");
+	die();
+}
 
 
 $url = "https://raidsiz.lima-city.de/m2t/gyms_iz.php";
@@ -15,7 +27,7 @@ $data = json_decode($homepage);
 foreach($data AS $dat)
 {
     echo "X".$dat->name." | ".$dat->image." | ".$dat->lat." | ".$dat->lon." | ".$dat->ex."<br>";
-    $res = my_query("SELECT * FROM gyms WHERE gym_id = '".$dat->portal_id."'");
+    $res = $db->query("SELECT * FROM gyms WHERE gym_id = '".$dat->portal_id."'");
     $row = $res->fetch_object();
     echo "Z".$row->name." | ".$row->img_url." | ".$dat->lat." | ".$dat->lon." | ".$dat->ex_gym."<br>";
 
