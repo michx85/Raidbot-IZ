cf9a8e85b331edc2e5cfa06816674ef0b3397187
BuildTools <unconfigured@null.spigotmc.org>
2020-04-19 23:06:02 +0200

    ...

--- core/tools/iz_import.php
+++ core/tools/iz_import.php
@@ -12,7 +12,7 @@ $data = json_decode($homepage);
 foreach($data AS $dat)
 {
     echo "X".$dat->name." | ".$dat->image." | ".$dat->lat." | ".$dat->lon." | ".$dat->ex."<br>";
-    $res = my_query("SELECT * FROM gym WHERE gym_id = '".$dat->portal_id."'");
+    $res = my_query("SELECT * FROM gyms WHERE gym_id = '".$dat->portal_id."'");
     $row = $res->fetch_object();
     echo "Z".$row->name." | ".$row->img_url." | ".$dat->lat." | ".$dat->lon." | ".$dat->ex_gym."<br>";
 
