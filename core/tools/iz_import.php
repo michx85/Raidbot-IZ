5cb480949f1f06576213425758065dbbf2ce7fb2
BuildTools <unconfigured@null.spigotmc.org>
2020-04-19 23:56:37 +0200

    ...

--- core/tools/iz_import.php
+++ core/tools/iz_import.php
@@ -3,7 +3,7 @@
 
 include_once(dirname(__DIR__). '/bot/requirements.php');
 
-echo VERSION;
+
 
 $url = "https://raidsiz.lima-city.de/m2t/gyms_iz.php";
 		// echo $url;
@@ -14,10 +14,10 @@ $data = json_decode($homepage);
 
 foreach($data AS $dat)
 {
-    #echo "X".$dat->name." | ".$dat->image." | ".$dat->lat." | ".$dat->lon." | ".$dat->ex."<br>";
-    #$res = my_query("SELECT * FROM gyms WHERE gym_id = '".$dat->portal_id."'");
-    #$row = $res->fetch_object();
-    #echo "Z".$row->name." | ".$row->img_url." | ".$dat->lat." | ".$dat->lon." | ".$dat->ex_gym."<br>";
+    echo "X".$dat->name." | ".$dat->image." | ".$dat->lat." | ".$dat->lon." | ".$dat->ex."<br>";
+    $res = my_query("SELECT * FROM gyms WHERE gym_id = '".$dat->portal_id."'");
+    $row = $res->fetch_object();
+    echo "Z".$row->name." | ".$row->img_url." | ".$dat->lat." | ".$dat->lon." | ".$dat->ex_gym."<br>";
 
 }
 
