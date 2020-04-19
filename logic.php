d441a5443762f7f4727cfb3119d2e06a4f2713f3
BuildTools <unconfigured@null.spigotmc.org>
2020-04-19 18:24:53 +0200

    ...

--- logic.php
+++ logic.php
@@ -2383,7 +2383,7 @@ function keys_vote($raid)
 
             // Zero Participants? Show only time buttons!
             if($count_pp == 0) {
-                $keys = $buttons_time;
+                $keys = array_merge($buttons_time,$button_bossinfo);
             } else {
                 // Init keys pokemon array.
                 $buttons_pokemon = [];
