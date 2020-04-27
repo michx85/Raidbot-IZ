9c9fc4a89189b8f8189dea9bf6171103f983c34d
BuildTools <unconfigured@null.spigotmc.org>
2020-04-28 01:33:55 +0200

    ...

--- mods/vote_status.php
+++ mods/vote_status.php
@@ -9,7 +9,7 @@ debug_log('vote_status()');
 // Check if the user has voted for this raid before.
 $rs = my_query(
     "
-    SELECT    user_id, attend_time, extra_valor+remote+extra_mystic+extra_instinct AS remote
+    SELECT    user_id, attend_time, remote
     FROM      attendance
       WHERE   raid_id = {$data['id']}
         AND   user_id = {$update['callback_query']['from']['id']}
